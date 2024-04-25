00c37ee9e4f04ec87f47705db449862123ca6b80
Adrian033001 <bejocadrian30@gmail.com>
2024-04-25 19:59:49 +0800

    more updates

--- js/validate.js
+++ js/validate.js
@@ -6,9 +6,9 @@ document.getElementById("my_form").addEventListener("submit", function(event) {
   var noCheckbox = document.getElementById("answer_no");
   if (yesCheckbox.checked) {
     document.querySelector('.results').innerText = 'Ayyeeeeiiiihhh i love you very much babbiii';
-  } else if (noCheckboxCheckbox.checked) {
+  } if (noCheckbox.checked) {
     document.querySelector('.results').innerText = 'Di mo ako lab?? huhuhuhuhur';
   }else {
-    document.querySelector('.results').innerText = 'Pamili ug usa sa choices, or ako imong pilion';
+    document.querySelector('.results').innerText = 'Pamili ug usa sa choices, or ako imong pilion ohohohoyy';
   }
 });
